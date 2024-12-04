b5b1ba108b2f6e26ebd0aa4c2f826ff6eb750f77
angx7 <angelmirocu@gmail.com>
2024-12-03 22:35:15 -0600

    alerts

--- js/login.js
+++ js/login.js
@@ -10,7 +10,13 @@ function verificar() {
   const contraseña = document.getElementById("contraseña").value;
 
   if (!usuario || !contraseña) {
-    alert("Ops, faltan datos");
+    Swal.fire({
+      position: "center",
+      icon: "warning",
+      title: "Faltan datos en el registro",
+      showConfirmButton: false,
+      timer: 1500,
+    });
     return;
   }
 
@@ -19,11 +25,43 @@ function verificar() {
   );
 
   if (usuarioEncontrado) {
-    alert("Bienvenido");
-    window.location.href = "home.html";
+    // alert("Bienvenido");
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Bienvenido",
+      showConfirmButton: true,
+    }).then(() => {
+      window.location.href = "home.html";
+    });
+    // window.location.href = "home.html";
   } else {
-    alert("Datos incorrectos contacta con el proveedor");
+    // alert("Datos incorrectos contacta con el proveedor");
+    Swal.fire({
+      position: "center",
+      icon: "error",
+      title: "Datos incorrectos",
+      text: "Contacta con el proveedor",
+      showConfirmButton: false,
+      timer: 2500,
+    });
     document.getElementById("usuario").value = "";
     document.getElementById("contraseña").value = "";
   }
 }
+async function forgotPassword() {
+  await Swal.fire({
+    icon: "info",
+    iconColor: "#7D3A2B",
+    title: "Olvidé mi contraseña",
+    text: "Por favor, contacta con soporte para restablecer tu contraseña.",
+    showConfirmButton: true,
+    confirmButtonColor: "#A6553B",
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("forgotPassword")
+    .addEventListener("click", forgotPassword);
+});
